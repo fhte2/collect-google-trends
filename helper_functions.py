61627206120f1032c2f649bef5257a0cd0c0a624
fhte2 <95410259+fhte2@users.noreply.github.com>
2022-08-22 19:43:14 +0200

    updating helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -7,10 +7,10 @@ def trim_month(month):
 
 def check_date(start_year, end_year, start_month, end_month):
     if end_year < start_year:
-        print('End year cannot be smaller than start year')
+        print('End year cannot be smaller than start year. Update the dates and try again.')
         return False
     elif (end_year == start_year ) and (end_month < start_month):
-        print('End month cannot be smaller than start month if start and end year are the same')
+        print('End month cannot be smaller than start month if start and end year are the same. Update the dates and try again.')
         return False
     else: 
         return True
